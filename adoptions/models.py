98363d1f06af37b1f6c6033c7b71150ce10f1a16
wayne71 <wayne.hatter1971@gmail.com>
2024-05-04 02:32:26 -0500

    add columns to database

--- adoptions/models.py
+++ adoptions/models.py
@@ -9,9 +9,12 @@ class Pet(models.Model):
     breed = models.CharField(max_length=30, blank=True)
     description = models.TextField()
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
-    subission_date = models.DateTimeField()
+    submission_date = models.DateTimeField()
     age = models.IntegerField()
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
     
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
